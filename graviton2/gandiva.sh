2893c3537bf1e92e02238588fdaa40cf7b8ddf76
Welly Tambunan <if05041@gmail.com>
2021-10-08 22:49:44 +0000

    adding orc to gandiva compilation

--- graviton2/gandiva.sh
+++ graviton2/gandiva.sh
@@ -4,6 +4,6 @@ tar xvf apache-arrow-4.0.0.tar.gz
 
 cd arrow-apache-arrow-4.0.0/cpp
 rm -rf release && mkdir release && cd release
-cmake .. -GNinja -DARROW_GANDIVA=ON -DARROW_GANDIVA_JAVA=ON -DARROW_DEPENDENCY_SOURCE=BUNDLED
+cmake .. -GNinja -DARROW_GANDIVA=ON -DARROW_GANDIVA_JAVA=ON -DARROW_ORC=ON -DARROW_DATASET=ON -DARROW_DEPENDENCY_SOURCE=BUNDLED
 ninja
 sudo ninja install
