a6ef9b8039931377c9c72ff4dc2bc6e7797277ac
Welly Tambunan <if05041@gmail.com>
2021-10-08 13:24:15 +0000

    fix arrow building

--- graviton2/gandiva.sh
+++ graviton2/gandiva.sh
@@ -2,7 +2,7 @@
 wget https://github.com/apache/arrow/archive/refs/tags/apache-arrow-4.0.0.tar.gz
 tar xvf apache-arrow-4.0.0.tar.gz
 
-cd apache-arrow-4.0/cpp
+cd arrow-apache-arrow-4.0.0/cpp
 rm -rf release && mkdir release && cd release
 cmake .. -GNinja -DARROW_GANDIVA=ON -DARROW_GANDIVA_JAVA=ON -DARROW_DEPENDENCY_SOURCE=BUNDLED
 ninja
