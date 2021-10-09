05fb745bd7c9dc97eb1ad6b4db73eb609ad74a3f
Welly Tambunan <if05041@gmail.com>
2021-10-09 02:38:51 +0000

    fix gandiva c++ library path

--- graviton2/arrow-java.sh
+++ graviton2/arrow-java.sh
@@ -5,4 +5,4 @@ git submodule update --init --recursive # Needed for flight
 # download the release tar.gz 
 cd java
 mvn install
-mvn install -P arrow-jni -am -Darrow.cpp.build.dir=/home/ubuntu/awsindo/dremio-gv2/graviton2/arrow-apache-arrow-4.0.0/cpp/release -Dlicense.skip=true -e
+mvn install -P arrow-jni -am -Darrow.cpp.build.dir=/home/ubuntu/awsindo/dremio-gv2/graviton2/arrow-apache-arrow-4.0.0/cpp/release/release -Dlicense.skip=true -e
